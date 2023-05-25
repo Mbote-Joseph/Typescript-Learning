cf2739040f677a2e6988f618b02b0f7341929242
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-05-25 23:45:37 +0300

    Learning Typescript

--- tuples.ts
+++ tuples.ts
@@ -1,2 +1,11 @@
-const users: (string | number)[] = ["huud", 2];
+// const users: (string | number)[] = ["huud", 2];
+
+let users: [string, number, boolean];
+
+users = ["Joseph", 12, true];
+
+console.log(users);
+
+let rgb: [number, number, number] = [255, 123, 112];
+
 export {};
