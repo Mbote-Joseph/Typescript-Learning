5cb2043ae7024f19d7f8f60610fd4101469fbc8b
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-05-26 22:36:49 +0300

    Learning Typescript

--- enums.ts
+++ enums.ts
@@ -1,1 +1,16 @@
+enum SeatChoice {
+  AISLE,
+  MIDDLE,
+  WINDOW,
+  FOURTH,
+}
+
+enum SeatChoices {
+  AISLE = 10,
+  MIDDLE = 22,
+  WINDOW,
+  FOURTH = 44,
+}
+const hcSeat = SeatChoice.MIDDLE;
+
 export {};
